1d1d3c10bbab58cfec950a34e2b9dc49ec0f6dd2
Sebastián <sebastianacaro6@gmail.com>
2026-05-11 00:29:01 -0500

    chore: Agrega la estructura del backend para el proyecto

---
+++ gitt-backend/src/app.module.ts
@@ -0,0 +1,47 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { AuthModule } from './auth/auth.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { ArticulosModule } from './articulos/articulos.module';
+import { CatalogosModule } from './catalogos/catalogos.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { PrestamosModule } from './prestamos/prestamos.module';
+import { MantenimientosModule } from './mantenimientos/mantenimientos.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: Number(configService.get<string>('DB_PORT')),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
+
+    AuthModule,
+    UsuariosModule,
+    ArticulosModule,
+    CatalogosModule,
+    DashboardModule,
+    PrestamosModule,
+    MantenimientosModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
